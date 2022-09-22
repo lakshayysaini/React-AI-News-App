70b3a75157eac995032e6a21387e1326f0e49e46
lakshayysaini <lakshayysaini@gmail.com>
2022-09-22 16:39:22 +0530

    Added Styles and made alan and website dynamic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 
+import useStyles from './styles.js';
+
 import NewsCards from "./components/NewsCards/NewsCards";
 
 const alankey ='781850a6b72e538fbd5d3544e3631a7e2e956eca572e1d8b807a3e2338fdd0dc/stage';
@@ -9,6 +11,9 @@ const alankey ='781850a6b72e538fbd5d3544e3631a7e2e956eca572e1d8b807a3e2338fdd0dc
 const App = () => {
 
     const [newsArticles, setNewsArticles] = useState([]);
+    const [activeArticle, setActiveArticle] = useState(-1);
+
+    const classes= useStyles();
 
     useEffect(() => {
         alanBtn({
@@ -16,6 +21,8 @@ const App = () => {
             onCommand: ({command, articles}) => {
                 if(command === 'newHeadlines'){
                     setNewsArticles(articles);
+                } else if(command === 'highlight'){
+                    setActiveArticle((prevActiveArticle) => prevActiveArticle +1);
                 }
             }
         })
@@ -23,8 +30,10 @@ const App = () => {
 
     return(
         <div>
-            <h1>Alan AI News Application</h1>
-            <NewsCards articles={newsArticles}/>
+            <div className={classes.logoContainer}>
+                <img src="https://i.ibb.co/qygzDzL/Alan.png" className={classes.alanLogo} alt="alan logo" />
+            </div>
+            <NewsCards articles={newsArticles} activeArticle={activeArticle}/>
         </div>
     );
 }
--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
+import classNames from 'classnames';
 
-const NewsCard = () => {
+import useStyles from './styles.js';
+
+
+const NewsCard = ({article :{description, publishedAt, source, title, url, urlToImage}, i, activeArticle}) => {
+
+  const classes = useStyles();
+
   return (
-    <div>
-      News Card
-    </div>
+    <Card className={classNames(classes.card, activeArticle === i ? classes.activeCard : null )}>
+      <CardActionArea href={url} target="_blank">
+          <CardMedia className={classes.media} image={urlToImage || 'https://thumbs.dreamstime.com/b/news-newspapers-folded-stacked-word-wooden-block-puzzle-dice-concept-newspaper-media-press-release-42301371.jpg'}/>
+          <div className={classes.details}>
+            <Typography variant="body2" color="textSecondary" component="h2" >{(new Date(publishedAt)).toDateString()}</Typography>
+            <Typography variant="body2" color="textSecondary" component="h2" >{source.name}</Typography>
+          </div>
+          <Typography className={classes.title} gutterBottom variant="h5">{title}</Typography>
+          <CardContent>
+            <Typography variant="body2" color="textSecondary" component="p">{description}</Typography>
+          </CardContent>
+      </CardActionArea>
+      <CardActions className={classes.cardActions}>
+        <Button size="small" color="primary">Read More</Button>
+        <Typography variant="h5" color="textSecondary">{i+1}</Typography>
+      </CardActions>
+    </Card>
   )
 }
 
