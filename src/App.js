bcb7b4abc052e88c70e662414f4704f63c6bc4c2
lakshayysaini <lakshayysaini@gmail.com>
2022-09-22 06:00:22 +0530

    implementing alan aith api keys

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React, {useEffect} from "react";
+import React, {useState, useEffect} from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
-import { useEffect } from "react";
+
+import NewsCards from "./components/NewsCards/NewsCards";
+
+const alankey ='781850a6b72e538fbd5d3544e3631a7e2e956eca572e1d8b807a3e2338fdd0dc/stage';
+
 
 const App = () => {
+
+    const [newsArticles, setNewsArticles] = useState([]);
+
+    useEffect(() => {
+        alanBtn({
+            key: alankey,
+            onCommand: ({command, articles}) => {
+                if(command === 'newHeadlines'){
+                    alert('This code was executed');
+                }
+            }
+        })
+    },[])
+
     return(
         <div>
             <h1>Alan AI News Application</h1>
+            <NewsCards articles={newsArticles}/>
         </div>
     );
 }
